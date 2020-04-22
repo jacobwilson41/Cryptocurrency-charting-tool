99cac28ded2e211a1fee0f0df8263c774dba7f50
Jacob Wilson <jacobcharles96@gmail.com>
2020-04-22 19:22:52 -0400

    Completed basics

--- app.jsx
+++ app.jsx
@@ -1,4 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Bar} from 'react-chartjs-2';
 
-ReactDOM.render(<h1>hello, world</h1>, document.getElementById('app'));
+const axios = require('axios');
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      labels: [],
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api')
+    .then(res => {
+      this.setState({
+        labels: Object.keys(res.data.bpi),
+        data: Object.values(res.data.bpi)
+      })
+      console.log(this.state)
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    const data = {
+      labels: this.state.labels,
+      datasets: [{
+        label: "My First dataset",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: this.state.data
+      }]
+    };
+    return (
+      <div>
+        <h1>Bitcoin Price</h1>
+        <Bar data={data}/>
+      </div>
+    )
+  }
+
+
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.use(express.static(__dirname + '/dist'));
+
+
+
+app.get('/api', (req, res) => {
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+  .then(response => res.send(response.data))
+  .catch(err => res.status(500));
+})
 
 const port = 3000;
 app.listen(port, ()=>console.log(`listening on port ${port}!`))
